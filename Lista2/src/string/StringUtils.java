ca92ef42dce7982bd90a738526dd68f272b5f220
Gabriel Brum <gabrielbrum2805@gmail.com>
2024-03-25 22:54:25 -0300

    adding StringUtils

--- Lista2/src/string/StringUtils.java
+++ Lista2/src/string/StringUtils.java
@@ -1,8 +1,25 @@
 package string;
 
+
 public class StringUtils {
+    // counter
     private static int actionsCount = 0;
 
+    public static void main(String[] args) {
+        String a = "Olá Mundo";
+
+        a.toUpperCase();
+
+        System.out.println(a);
+    }
+
     // methods
-    public
+    String toUp() {
+        String str;
+        char []s = str.toCharArray();
+        
+
+        return str;
+        actionsCount++;
+    }
 }
